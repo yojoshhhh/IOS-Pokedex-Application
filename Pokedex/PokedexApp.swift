e972db59506d944009b7c62cd735b62f02c059d6
Joshua Austin <ja942822@wcupa.edu>
2023-12-11 23:09:19 -0500

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Joshua Austin on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
